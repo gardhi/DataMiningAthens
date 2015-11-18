74c3d39bea8e823198d8ab3c8182ac30571076ea
Gardhi <gard.hillestad@gmail.com>
2015-11-18 18:39:16 +0100

    filter: remove duplicate hashtagsets

--- ProjectBin/clean.py
+++ ProjectBin/clean.py
@@ -6,23 +6,35 @@ Created on Wed Nov 18 14:32:14 2015
 """
 
 import json
+import time
+
+start_time = time.time()
+unique_tagset = set()
 
 
+#filtering data
 for date in range(1,32):
     filename = "Paris-2015-1-" + str(date)
     json_data = open("/cal/exterieurs/ext6641/ParisSearchJan/" + filename)
     
-    ut = open("/cal/exterieurs/ext6641/ProjectDump/" + filename, "wb")
+    ut = open("/cal/exterieurs/ext6641/DataMiningAthens/ParisSearchJanFiltered/" + filename, "wb")
     
-    for line in json_data:
+    for line in json_data:        
         data = json.loads(line)
-        utdata = "{\"id\": "  + str(data["id"]) + ", \"hashtags\": ["
+        
         if(data["entities"]["hashtags"]):
-            for tags in data["entities"]["hashtags"]:
-                utdata += tags["text"].lower() + ", "    
-            utdata = utdata[:-2]
-            utdata += "], \"text\":" + data["text"].replace("\n",' ') + "}\n"
-            ut.write(utdata)
-    ut.close()
+            utdata = {"id" : data["id"], "hashtags": [], "text" : data["text"], "user_id" : data["user"]["id"]}
+            for tags in data["entities"]["hashtags"]:                
+                utdata['hashtags'].append(tags["text"].lower())
+            
+            if frozenset(utdata["hashtags"]) not in unique_tagset:
 
-print "finished"
+                unique_tagset.add(frozenset(utdata["hashtags"]))
+                ut.write(json.dumps((utdata), indent=4, separators=(',',': '), sort_keys=True).replace("\n",''))
+                ut.write('\n')
+            
+    ut.close()
+    json_data.close()
+
+print "Finished Filtering"
+print("it took: %s seconds" % (time.time() - start_time))
--- ProjectBin/test.py
+++ ProjectBin/test.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 
 # Open a file
-fo = open("/cal/exterieurs/ext6641/test.txt","r")
-print "Name of the file: ", fo.name
-print "Closed or not : ", fo.closed
-print "Opening mode : ", fo.mode
-print "Softspace flag : ", fo.softspace
 
-print 
-fo.read()
+import json
 
+filename = "Paris-2015-1-1"
+ut = open("/cal/exterieurs/ext6641/test/" + filename, "r")
 
+for line in ut:
+    print line
+    hallo = json.loads(line)
 
+ut.close()
