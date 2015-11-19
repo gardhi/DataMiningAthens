71d4f5c679384f692afb0ec8e491efda4e09fa67
Gardhi <gard.hillestad@gmail.com>
2015-11-19 15:12:03 +0100

    Remove single hashtag tweets

--- ProjectBin/filter.py
+++ ProjectBin/filter.py
@@ -11,15 +11,18 @@ import time
 start_time = time.time()
 unique_tagset = set()
 
+num_tweets = 0
 
 #filtering data
 for date in range(1,32):
     filename = "Paris-2015-1-" + str(date)
     json_data = open("/cal/exterieurs/ext6641/ParisSearchJan/" + filename)
     
-    ut = open("/cal/exterieurs/ext6641/DataMiningAthens/ParisSearchJanFiltered/" + filename, "wb")
+    ut = open("/cal/exterieurs/ext6641/DataMiningAthens/FilteredData/ParisSearchJanFiltered/" + filename, "wb") # test/" +filename,"wb") ##
     
-    for line in json_data:        
+    for line in json_data: 
+        num_tweets += 1        
+        
         data = json.loads(line)
         
         if(data["entities"]["hashtags"]):
@@ -27,14 +30,15 @@ for date in range(1,32):
             for tags in data["entities"]["hashtags"]:                
                 utdata['hashtags'].append(tags["text"].lower())
             
-            if frozenset(utdata["hashtags"]) not in unique_tagset:
+            if (len(utdata["hashtags"]) > 1):
+                
+                if frozenset(utdata["hashtags"]) in unique_tagset:
+                    unique_tagset.add(frozenset(utdata["hashtags"]))
+                    ut.write(json.dumps((utdata), indent=4, separators=(',',': '), sort_keys=True).replace("\n",''))
+                    ut.write('\n')
 
-                unique_tagset.add(frozenset(utdata["hashtags"]))
-                ut.write(json.dumps((utdata), indent=4, separators=(',',': '), sort_keys=True).replace("\n",''))
-                ut.write('\n')
-            
     ut.close()
     json_data.close()
 
-print "Finished Filtering"
+print "Finished Filtering " + num_tweets + " tweets."
 print("it took: %s seconds" % (time.time() - start_time))
--- ProjectBin/test.py
+++ ProjectBin/test.py
@@ -1,14 +1,50 @@
 #!/usr/bin/python
 
 # Open a file
+#
+#import json
+#
+#filename = "Paris-2015-1-1"
+#ut = open("/cal/exterieurs/ext6641/test/" + filename, "r")
+#
+#for line in ut:
+#    print line
+#    hallo = json.loads(line)
+#
+#ut.close()
 
-import json
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
-filename = "Paris-2015-1-1"
-ut = open("/cal/exterieurs/ext6641/test/" + filename, "r")
 
-for line in ut:
-    print line
-    hallo = json.loads(line)
+def update_line(num, data, line):
+    line.set_data(data[..., :num])
+    return line,
 
-ut.close()
+fig1 = plt.figure()
+
+data = np.random.rand(2, 25)
+l, = plt.plot([], [], 'r-')
+plt.xlim(0, 1)
+plt.ylim(0, 1)
+plt.xlabel('x')
+plt.title('test')
+line_ani = animation.FuncAnimation(fig1, update_line, 25, fargs=(data, l),
+                                   interval=50, blit=True)
+#line_ani.save('lines.mp4')
+
+fig2 = plt.figure()
+
+x = np.arange(-9, 10)
+y = np.arange(-9, 10).reshape(-1, 1)
+base = np.hypot(x, y)
+ims = []
+for add in np.arange(15):
+    ims.append((plt.pcolor(x, y, base + add, norm=plt.Normalize(0, 30)),))
+
+im_ani = animation.ArtistAnimation(fig2, ims, interval=50, repeat_delay=3000,
+                                   blit=True)
+#im_ani.save('im.mp4', metadata={'artist':'Guido'})
+
+plt.show()
